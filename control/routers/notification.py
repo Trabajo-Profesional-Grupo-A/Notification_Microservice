a1bbafeae9095e0cdf28de668a66874163ee3e0e
nathencinoza <nencinoza@fi.uba.ar>
2024-08-08 21:20:21 -0300

    .

--- control/routers/notification.py
+++ control/routers/notification.py
@@ -1,14 +1,27 @@
 """
 This module contains the API endpoints for the notifications service.
 """
-
+import os
+import json
+import base64
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
 
 
-cred = credentials.Certificate("./tpp-grupoa-firebase-adminsdk-5jdgm-65be1e639d.json")
-firebase_admin.initialize_app(cred)
+#cred = credentials.Certificate("./tpp-grupoa-firebase-adminsdk-5jdgm-65be1e639d.json")
+#firebase_admin.initialize_app(cred)
+
+def initialize_firebase():
+    if not firebase_admin._apps:
+        # Decode the base64-encoded service account key
+        service_account_info = json.loads(base64.b64decode(os.environ.get('FIREBASE_SERVICE_ACCOUNT_KEY')).decode('utf-8'))
+        
+        # Use the decoded service account info to initialize Firebase
+        cred = credentials.Certificate(service_account_info)
+        firebase_admin.initialize_app(cred)
+
+
 
 from fastapi import (
     APIRouter,
